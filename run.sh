b81357546c29c5906c9c46e0c3ae61ef6c091370
narkisr <narkisr@gmail.com>
2013-02-19 01:25:50 +0200

    initial sandbox import

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-puppet apply --modulepath=modules:static-modules manifests/default.pp\
-  --node_terminus exec --external_nodes=/vagrant/lookup.sh --hiera_config hiera.yaml $@
+puppet apply --modulepath=modules:static-modules manifests/default.pp --hiera_config hiera.yaml $@
