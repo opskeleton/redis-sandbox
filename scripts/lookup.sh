dde467707a2e774cad22f1260377470890b724c0
narkisr <narkisr@gmail.com>
2013-02-19 02:07:57 +0200

    updating gems

--- scripts/lookup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export ip=`echo $SSH_CONNECTION |  cut -d  ' ' -f 1`
-wget -q -O -  "http://$ip:8080/registry/profile/$1"
--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-puppet apply --modulepath=modules:static-modules manifests/default.pp\
-  --node_terminus exec --external_nodes=`pwd`/scripts/lookup.sh --hiera_config hiera.yaml $@
-exit 0;
